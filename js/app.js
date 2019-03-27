64ad2359dfd7b8b6438e1adfe9ffad778661d173
Just-Hussain <1Just.Hussain@gmail.com>
2019-03-28 01:51:06 +0300

    added data structure, randomized elements in html, added listenner and handeling for matching and mis-matching.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
  */
 
 
+let cards = Array.from(document.getElementsByClassName("card"));
+
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -10,20 +13,46 @@
  *   - add each card's HTML to the page
  */
 
+shuffleHtml();
+
+
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+// function shuffleArray(array) {
+//     var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+//     while (currentIndex !== 0) {
+//         randomIndex = Math.floor(Math.random() * currentIndex);
+//         currentIndex -= 1;
+//         temporaryValue = array[currentIndex];
+//         array[currentIndex] = array[randomIndex];
+//         array[randomIndex] = temporaryValue;
+//     }
+
+//     return array;
+// }
+
+function shuffleHtml() {
+    
+    let cardsNames = [
+        "fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o",
+        "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt",
+        "fa-cube", "fa-cube", "fa-leaf", "fa-leaf",
+        "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"
+    ];
+
+    for (let i = 15; i > -1; i--) {
+        const random = Math.floor(Math.random() * i);  
+        
+        cards[i].children[0].className = "fa " + cardsNames[random];
+        
+        cardsNames.splice(random, 1);
     }
-
-    return array;
 }
+
+
+
+
+
 
 
 /*
@@ -36,3 +65,49 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ let openCards = [];
+
+ document.querySelector(".deck").addEventListener("click", function(event) {
+    const parentClass = event.target.className; 
+    
+
+    if (parentClass == "card") {
+        const childClass = event.target.children[0].className;
+
+        
+        if (openCards.length == 0) {
+            let info = {
+                child: childClass,
+                parent: event.target
+            };
+            openCards.push(info);
+            
+            event.target.className = "card open show";
+        }
+        else if (openCards.length > 0) {
+            let info = {
+                child: childClass,
+                parent: event.target
+            };
+
+            openCards.push(info);
+                        
+            if (openCards[0].child == openCards[1].child) {
+                openCards[0].parent.className = "card match";
+                openCards[1].parent.className = "card match";
+            }
+            else {
+                openCards[0].parent.className = "card";
+                openCards[1].parent.className = "card";
+            }
+
+            openCards.pop();
+            openCards.pop();
+        }
+
+        console.log(event.target.children[0]);
+        
+    }
+ });
+ 
